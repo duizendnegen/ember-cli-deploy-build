b3ef23060af25a2f620ecb83d3a32c05580b43b3
Pepijn Schoen <pepijn@eliandor.com>
2017-05-04 07:39:31 +0200

    Rely on using exec over a project that is passed to increase decoupling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var RSVP = require('rsvp');
 var glob  = require('glob');
 var DeployPluginBase = require('ember-cli-deploy-plugin');
 var path = require('path');
+const exec = require('child_process').exec;
 
 module.exports = {
   name: 'ember-cli-deploy-build',
@@ -22,32 +23,35 @@ module.exports = {
         var outputPath = this.readConfig('outputPath');
         var buildEnv   = this.readConfig('environment');
 
-        var Builder  = this.project.require('ember-cli/lib/models/builder');
-        var builder = new Builder({
-          ui: this.ui,
-          outputPath: outputPath,
-          environment: buildEnv,
-          project: this.project
-        });
-
         this.log('building app to `' + outputPath + '` using buildEnv `' + buildEnv + '`...', { verbose: true });
-        return builder.build()
-          .finally(function() {
-            return builder.cleanup();
-          })
-          .then(this._logSuccess.bind(this, outputPath))
-          .then(function(files) {
-            files = files || [];
 
-            return {
-              distDir: outputPath,
-              distFiles: files
-            };
-          })
-          .catch(function(error) {
-            self.log('build failed', { color: 'red' });
-            return RSVP.reject(error);
-          });
+        return new RSVP.Promise(function(resolve, reject) {
+          exec('ember build --environment ' + buildEnv + ' --output-path ' + outputPath,
+            {maxBuffer: 1024 * 1024 * 32},
+            function(err, stdout, stderr)
+          {
+            if(err) {
+              this.log(err, { color: 'red' });
+              reject(err);
+              return;
+            }
+
+            resolve(outputPath);
+          }.bind(this));
+        }.bind(this))
+        .then(this._logSuccess.bind(this, outputPath))
+        .then(function(files) {
+          files = files || [];
+
+          return {
+            distDir: outputPath,
+            distFiles: files
+          };
+        })
+        .catch(function(error) {
+          this.log('build failed', { color: 'red' });
+          return RSVP.reject(error);
+        }.bind(this));
       },
       _logSuccess: function(outputPath) {
         var self = this;
